129bbca4d7d3302140c3a3c08249c3c011ca0b3c
CARLOS GONZALEZ ANAYA <carlosgonzalesanaya@gmail.com>
2026-03-07 21:29:03 -0500

    Year Leap

---
+++ LogicConcepts/LeapYear/Program.cs
@@ -0,0 +1,52 @@
+﻿using Shared;
+
+do
+{
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = ConsoleExtension.GetInt("Ingrese el año a validar: ");
+
+
+    if (year == currentYear)
+    {
+        message = "es";
+    }
+    else if (year > currentYear)
+    {
+        message = "va a ser";
+    }
+    else
+    {
+        message = "fue";
+    }
+
+
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year%400==0)
+            {
+                Console.WriteLine($"El año: {year}, SI {message} bisiesto");
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine($"El año: {year}, NO {message} bisiesto");
+                Console.WriteLine("");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"El año: {year}, SI {message} bisiesto");
+            Console.WriteLine("");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El año: {year}, NO {message} bisiesto");
+        Console.WriteLine("");
+    }
+} while (true);
+
+
