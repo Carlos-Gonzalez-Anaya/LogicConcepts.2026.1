56ec387f45df346fc0c12924a8e7cb9f28c1dfad
CARLOS GONZALEZ ANAYA <carlosgonzalesanaya@gmail.com>
2026-03-08 07:38:41 -0500

    Serie Taylor modifique

---
+++ LogicConcepts/serieTaylorModifique/Program.cs
@@ -0,0 +1,29 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Cauntos terminos deseas: ");
+    var x = ConsoleExtension.GetDouble("digita el valor de x: ");
+    var taylor = Taylor(x, n);
+    Console.WriteLine($"f({x})={taylor:N5}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptionts("¿Deseas continuar [S]i, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+double Taylor(double x, int n)
+{
+    double sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += Math.Pow(-1,i)*(Math.Pow(x, i) / Mymath.factorial(i));
+    }
+    return sum;
+}
