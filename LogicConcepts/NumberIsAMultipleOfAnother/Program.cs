8cad54a5a5d2432d0b807e5438843d50cce90d17
CARLOS GONZALEZ ANAYA <carlosgonzalesanaya@gmail.com>
2026-03-07 20:25:56 -0500

    Number Is a Multiple Of Another

---
+++ LogicConcepts/NumberIsAMultipleOfAnother/Program.cs
@@ -0,0 +1,26 @@
+﻿using Shared;
+
+do
+{
+    var n1 = ConsoleExtension.GetInt("Ingrese el primer número: ");
+    var n2 = ConsoleExtension.GetInt("Ingrese el segundo número: ");
+    Console.WriteLine("");
+
+    if (n1 < n2)
+    {
+        Console.WriteLine("Por favor que el primer número sea mayor que sel segundo");
+        Console.WriteLine("");
+        continue;
+    }
+
+    if (n1%n2==0)
+    {
+        Console.WriteLine($"El número: {n2}, es multiplo de: {n1}");
+    }
+    else
+    {
+        Console.WriteLine($"El número: {n2}, no es multiplo de: {n1}");
+    }
+
+} while (true);
+
