dcd059f9957666a39b1ddfd242caef64ead4da92
CARLOS GONZALEZ ANAYA <carlosgonzalesanaya@gmail.com>
2026-03-08 07:17:11 -0500

    Factorial

---
+++ LogicConcepts/Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using Shared;
+using System.Net.Http.Headers;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("ingrese numero: ");
+    var factorial = Mymath.factorial(n);
+
+    Console.WriteLine($"{n}!={factorial:N0}");
+    Console.WriteLine("");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptionts("¿Deseas continuar [S]i, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
---
+++ LogicConcepts/Shared/Mymath.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared
+{
+    public class Mymath
+    {
+        public static double factorial(int n)
+        {
+            double factorial = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
