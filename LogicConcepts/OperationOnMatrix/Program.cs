30a689252375b2559f9c871a5a4da3ac99b514c4
CARLOS GONZALEZ ANAYA <carlosgonzalesanaya@gmail.com>
2026-03-08 12:42:17 -0500

    Create operation on a matrix

---
+++ LogicConcepts/OperationOnMatrix/Program.cs
@@ -0,0 +1,45 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Ingrese el orden de la matriz: ");
+    int menor = 9999;
+    int mayor = -9999;
+    int suma = 0;
+
+    int[,] matriz = new int[n,n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matriz[i, j] = (i + 1) - j;
+            Console.Write(matriz[i, j] + "\t");
+            
+            if (matriz[i,j]<menor)
+            {
+                menor = matriz[i,j];
+            }
+            if (matriz[i,j]>mayor)
+            {
+                mayor = matriz[i,j];
+            }
+            suma = matriz[i,j]+suma;
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine(matriz);
+    Console.WriteLine($"La sumatoria es :{suma} ");
+    Console.WriteLine($"el valor maximo es : {mayor}");
+    Console.WriteLine($"el valor minimo es : {menor}" );
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptionts("¿Deseas continuar [S]i, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
