7be2d5c3a73f8b78278106405a39657ba6344762
CARLOS GONZALEZ ANAYA <carlosgonzalesanaya@gmail.com>
2026-03-08 13:18:59 -0500

    Is prime

---
+++ LogicConcepts/IsPrime/Program.cs
@@ -0,0 +1,20 @@
+﻿using Shared;
+
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Ingresa numero: ");
+    var isPrime = Mymath.IsPrime(n);
+    Console.WriteLine($"El número: {n} {(isPrime ? "SI":"NO")} es primo");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptionts("¿Deseas continuar [S]i, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
