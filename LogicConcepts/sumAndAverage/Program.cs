00bbb2e9dbbb7e576badeb2ed3f1d5453575efef
CARLOS GONZALEZ ANAYA <carlosgonzalesanaya@gmail.com>
2026-03-08 07:00:13 -0500

    Sum and Average

---
+++ LogicConcepts/sumAndAverage/Program.cs
@@ -0,0 +1,27 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("cuantos numeros deseas: ");
+    int sum = 0;
+
+    for (int i = 1; i<=n; i++)
+    {
+        Console.Write($"{i}\t");
+        sum+=i;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"La suma es....{sum,20:N0}");
+    Console.WriteLine($"El promedio es{sum/n,20:N2}");
+
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptionts("¿Deseas continuar [S]i, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
