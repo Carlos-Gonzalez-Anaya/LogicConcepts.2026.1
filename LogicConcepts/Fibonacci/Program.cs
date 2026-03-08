f25a4f3d49c4c7941c60e7e9e959ea3767231217
CARLOS GONZALEZ ANAYA <carlosgonzalesanaya@gmail.com>
2026-03-08 08:23:21 -0500

    Serie fibonacci

---
+++ LogicConcepts/Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using Shared;
+using System.Runtime.Serialization;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Cuantos terminos quieres; ");
+    double a = 0;
+    double b = 1;
+
+    Console.Write($"{a}\t{b}\t");
+    for (int i = 2; i < n; i++)
+    {
+        
+        double c =a+b;
+        Console.Write($"{c}\t");
+        a =b;
+        b=c;
+        ;
+    }
+    Console.WriteLine("");
+
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptionts("¿Deseas continuar [S]i, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
